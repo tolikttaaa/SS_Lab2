193e0413c148c63fbb24c0c65eab8b5d6a9c6330
Anatoliy Anischenko <tolik.ttaaa@gmail.com>
2021-06-23 12:38:39 +0300

    Fix project name

--- ListModeLib.c
+++ ListModeLib.c
@@ -5,6 +5,7 @@
 #include "list_mode.h"
 
 JNIEXPORT jstring JNICALL Java_ListModeLib_runListMode(JNIEnv *jenv, jobject jobj) {
+    fprintf(stderr, "I'm HERE!!!!!!!!");
     char *msg = run_list_mode();
     return (*jenv)->NewStringUTF(jenv, msg);
 }
